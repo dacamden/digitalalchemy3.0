f0958be8c3c1e342be72a47abe6fd4fdd55bf175
slowsushi <wuytsky@gmail.com>
2014-07-10 15:09:17 +0100

    Create site.js

---
+++ week3/site.js
@@ -0,0 +1,20 @@
+var topics = {'1':"travel",'2':"news",'3':"football"};
+
+
+  $.each( topics, function( i, val ){
+
+    
+
+    $.ajax({
+
+      url: "http://content.guardianapis.com/search?q="+ val +"&show-fields=trailText%2CshortUrl",
+
+      dataType: "jsonp",
+
+      success: function(data) {
+        $.each(data.response.results, function () {
+                 $( "#" + val ).append('<li><a target="_blank" href="' + this['fields'].shortUrl + '">' + this['webTitle'] +'</a><p>' + this['fields'].trailText + '</p></li>');
+        });
+
+      }});
+  });
